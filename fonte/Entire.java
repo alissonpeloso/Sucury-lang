9f4cb1ecda49e663174d166c77828eddd9c4221b
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2020-11-11 10:16:31 -0300

    del troll

--- fonte/Entire.java
+++
@@ -1,9 +0,0 @@
-public class Entire extends Variable {
-    public int valor;
-
-    public Entire(String name, int valor){
-        this.type = "int";
-        this.name = name;
-        this.valor = valor;
-    }
-}
--- fonte/Float.java
+++
@@ -1,9 +0,0 @@
-public class Float extends Variable {
-    public float valor;
-
-    public Float(String name, float valor){
-        this.type = "float";
-        this.name = name;
-        this.valor = valor;
-    }
-}
