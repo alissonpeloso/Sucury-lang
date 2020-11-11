d104466fb6d68a76ebfa1fcf86c7c50fdb7283b0
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2020-11-11 09:56:38 -0300

    Add objct inteiro

---
+++ fonte/Entire.java
@@ -0,0 +1,9 @@
+public class Entire extends Variable {
+    public int valor;
+
+    public Entire(String name, int valor){
+        this.type = "int";
+        this.name = name;
+        this.valor = valor;
+    }
+}
