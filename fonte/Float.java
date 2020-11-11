704837365891cd034b5d63ad17476705adb85434
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2020-11-11 10:00:25 -0300

    ajuste type float

--- fonte/Float.java
+++ fonte/Float.java
@@ -2,7 +2,7 @@ public class Float extends Variable {
     public float valor;
 
     public Float(String name, float valor){
-        this.type = "int";
+        this.type = "float";
         this.name = name;
         this.valor = valor;
     }
