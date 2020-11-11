dbb5b49045580720387d30454c59f40826b9eb3f
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2020-11-11 10:15:39 -0300

    add float

---
+++ fonte/VarFloat.java
@@ -0,0 +1,9 @@
+public class VarFloat extends Variable {
+    public float valor;
+
+    public VarFloat(String name, float valor){
+        this.type = "float";
+        this.name = name;
+        this.valor = valor;
+    }
+}
