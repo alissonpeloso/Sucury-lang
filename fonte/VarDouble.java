30a54247ed8952e87e936255715e5a32129c19ec
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2020-11-11 10:15:29 -0300

    add double

---
+++ fonte/VarDouble.java
@@ -0,0 +1,9 @@
+public class VarDouble extends Variable{
+    public double valor;
+
+    public VarDouble(String name, double valor){
+        this.type = "double";
+        this.name = name;
+        this.valor = valor;
+    }
+}
