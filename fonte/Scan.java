db67a34dac698c7f10a7af9c478abfbd87b9d656
Alisson Luan de Lima Peloso <alisson.luan2000@gmail.com>
2020-11-11 12:08:57 -0300

    update using appendArray

--- fonte/Scan.java
+++ fonte/Scan.java
@@ -3,26 +3,26 @@ import java.util.Scanner;
 
 public class Scan {
     String lines[];
+    int numbLines;
 
     public void printFile(){
         for (int i = 0; i < this.lines.length ; i++) {
             System.out.println("Linha " + i + ": " + this.lines[i]);
         }
     }
+
     public void readFile(String fileName){
         try {
-            this.lines = new String[2000];
+            this.lines = new String[0];
             int amount = 0;
             File file = new File(fileName);
             Scanner input = new Scanner(file);
             while (input.hasNextLine()) {
                 String line = input.nextLine();
-                this.lines[amount++] = line;
+                this.lines = Util.AppendArray(this.lines.length, this.lines, line);
+                amount++;
             }
             input.close();
-            for (int i = 0; i < amount; i++) {
-                System.out.println("Linha " + i + ": " + this.lines[i]);
-            }
         } catch (Exception e) {
             System.out.println("Nao foi possivel abrir o arquivo "+fileName+".");
             e.printStackTrace();
